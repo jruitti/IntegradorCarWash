8f068e475336c4e75d7b860faf075434adef6d0d
Luc4s <luccas.a.pe@gmail.com>
2019-11-14 01:13:32 -0300

    Se Corrigió el Caso de Uso Modificar Turno

--- carWash-Core/src/main/java/interactor/ModificarTurnoUseCase.java
+++ carWash-Core/src/main/java/interactor/ModificarTurnoUseCase.java
@@ -12,23 +12,19 @@ import java.time.LocalDate;
 public class ModificarTurnoUseCase {
 
     private IModificarTurnoRepo modificarTurnoRepo;
-    private IRepositorioCrearVehiculo repositorioCrearVehiculo;
 
-    public ModificarTurnoUseCase(IModificarTurnoRepo modificarTurnoRepo, IRepositorioCrearVehiculo repositorioCrearVehiculo) {
+    public ModificarTurnoUseCase(IModificarTurnoRepo modificarTurnoRepo) {
         this.modificarTurnoRepo=modificarTurnoRepo;
-        this.repositorioCrearVehiculo=repositorioCrearVehiculo;
+
     }
 
     public boolean modificarTurno(Turno turnoDatosNuevo) throws TurnoExisteException {
 
-        //Vehiculo vehiculo =modificarTurnoRepo.findMatricula(turnoDatosNuevo.getVehiculo().getMatricula());
 
-        Vehiculo vehiculo = repositorioCrearVehiculo.findByMatricula(turnoDatosNuevo.getVehiculo().getMatricula());
-        Turno fecha = modificarTurnoRepo.findBFecha(turnoDatosNuevo.getFecha());
-       // Turno turno=modificarTurnoRepo.optenerId();
-        if(vehiculo==null||fecha==null){
+        Turno turnoAModificar=modificarTurnoRepo.findByVehiculoAndFecha(turnoDatosNuevo.getVehiculo().getMatricula(),turnoDatosNuevo.getFecha());
+        if(turnoAModificar==null){
             return this.modificarTurnoRepo.modificarTurno(turnoDatosNuevo);
-        }else if(fecha.getIdPedido()!=turnoDatosNuevo.getIdPedido()){
+        }else if(turnoAModificar.getIdPedido()!=turnoDatosNuevo.getIdPedido()){
             throw new TurnoExisteException("Id distintos no se puede modificar el turno");
         }else return this.modificarTurnoRepo.modificarTurno(turnoDatosNuevo);
 
