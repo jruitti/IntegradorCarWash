4b973b545e211d98cbcb59eb2677d2703d2b7262
Matias de la Fuente <matiasdlf90@gmail.com>
2019-11-18 15:13:18 -0300

    Se completó Service test de Modificar vehículo
    Se agregó modificarVehiculo_VehiculoExiste_DevuelveFalse

--- carWash-Boundaries/src/test/java/ar/edu/undec/ServiceIntegrationTest/ModificarVehiculoServiceIT.java
+++ carWash-Boundaries/src/test/java/ar/edu/undec/ServiceIntegrationTest/ModificarVehiculoServiceIT.java
@@ -26,10 +26,19 @@ public class ModificarVehiculoServiceIT {
     @Test
     public void modificarVehiculo_VehiculoModificado_DevuelveTrue() throws VehiculoExisteException {
         ClienteDTO elClienteDTO = new ClienteDTO(1, "Matías de la Fuente","La Mejicana 56","Cable Carril","34724669");
-        VehiculoDTO vehiculoAModificar = new VehiculoDTO(1, "NRP374","Toyota Etios","2016", elClienteDTO);
+        VehiculoDTO vehiculoAModificar = new VehiculoDTO(1, "NRP374","Toyota Etios","2018", elClienteDTO);
         when(iModificarVehiculoInput.modificarVehiculo(any(Vehiculo.class))).thenReturn(true);
         ModificarVehiculoController modificarVehiculoController = new ModificarVehiculoController(iModificarVehiculoInput);
         assertEquals(modificarVehiculoController.modificarVehiculo(vehiculoAModificar).getStatusCodeValue(), org.apache.http.HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void modificarVehiculo_VehiculoExiste_DevuelveFalse() throws VehiculoExisteException {
+        ClienteDTO elClienteDTO = new ClienteDTO(1, "Matías de la Fuente","La Mejicana 56","Cable Carril","34724669");
+        VehiculoDTO vehiculoAModificar = new VehiculoDTO(1, "NRP374","Toyota Etios","2018", elClienteDTO);
+        when(iModificarVehiculoInput.modificarVehiculo(any(Vehiculo.class))).thenThrow(new VehiculoExisteException("Vehiculo existe"));
+        ModificarVehiculoController modificarVehiculoController = new ModificarVehiculoController(iModificarVehiculoInput);
+        assertEquals(modificarVehiculoController.modificarVehiculo(vehiculoAModificar).getStatusCodeValue(), org.apache.http.HttpStatus.SC_PRECONDITION_FAILED);
     }
 
 }
