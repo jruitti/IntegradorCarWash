dd29c66457aaabb818f4a976f7fe4cfeb1ff3701
Matias de la Fuente <matiasdlf90@gmail.com>
2019-11-18 15:56:58 -0300

    Se creo controller para ObtenerVehiculoPorMatriculaController

--- carWash-Boundaries/src/main/java/ar/edu/undec/Service/Controller/ObtenerVehiculoPorMatriculaController.java
+++ carWash-Boundaries/src/main/java/ar/edu/undec/Service/Controller/ObtenerVehiculoPorMatriculaController.java
@@ -1,9 +1,10 @@
 package ar.edu.undec.Service.Controller;
 
 import ar.edu.undec.Service.ModeloService.VehiculoDTO;
+import ar.edu.undec.Service.ServiceMapper.VehiculoDTOMapper;
 import input.IObtenerVehiculoPorMatriculaImput;
-import modelo.Vehiculo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,16 +22,19 @@ public class ObtenerVehiculoPorMatriculaController {
     public ObtenerVehiculoPorMatriculaController(IObtenerVehiculoPorMatriculaImput obtenerVehiculoPorMatriculaImput){
         this.obtenerVehiculoPorMatriculaImput = obtenerVehiculoPorMatriculaImput;
     }
-    //@RequestMapping(value = "vehiculo/matricula{matricula}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    //@ResponseBody
+    @RequestMapping(value = "vehiculo/matricula{matricula}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
 
-    //public ResponseEntity<?> consultarVehiculoPorMatricula(@PathVariable("matricula") String matricula) {
-     //   try{
-       //     List<VehiculoDTO> vehiculos = new ArrayList<VehiculoDTO>();
-        //}
+    public ResponseEntity<?> consultarVehiculoPorMatricula(@PathVariable("matricula") String matricula) {
+        try{
+            List<VehiculoDTO> vehiculos = new ArrayList<>();
+            this.obtenerVehiculoPorMatriculaImput.buscarVehiculoPorMatricula(matricula).forEach(tp -> vehiculos.add(new VehiculoDTOMapper().mapeoCoreDTO(tp)));
+            if (vehiculos.isEmpty()) return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return ResponseEntity.status(HttpStatus.OK).body(vehiculos);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-
-
-
 
 }
